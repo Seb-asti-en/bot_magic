7ed30734d29e8d7172588133aad0bdaafbd656ba
Léo <leo.moreaux@etudiant.univ-reims.fr>
2021-04-14 17:32:41 +0200

    Connection again parce que conflits

--- code/client.py
+++ code/client.py
@@ -6,21 +6,8 @@ from card import Card
 HOST = 'localhost'  # The server's hostname or IP address
 PORT = 5000        # The port used by the server
 
-<<<<<<< Updated upstream
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect((HOST, PORT))
-    
-
-
-	# Create an instance of ProcessData() to send to server.
-	variable = Card()
-	# Pickle the object and send it to the server
-	data_string = pickle.dumps(variable)
-	s.send(data_string)
-=======
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_client:
 	socket_client.connect((HOST, PORT))
->>>>>>> Stashed changes
 
 	card = Card("hello", 3)
 	data_string = pickle.dumps(card)
