d509a44cecfc21609f3fc0974452b68fd9a82f34
seb <sebastien.boissier@etudiant.univ-reims.fr>
2021-04-14 14:34:45 +0200

    server files

--- code/server.py
+++ code/server.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
-import socket
+import socket, pickle
+from card import Card
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+	s.bind((HOST, PORT))
+	s.listen()
+	conn, addr = s.accept()
+	
+	
+	with conn:
+		print('Connected by', addr)
+		while True:
+
+			data = conn.recv(4096)
+			if not data:
+				break
+			data_variable = pickle.loads(data)
+			print (data_variable)
+			conn.send(data)
+				
+		conn.close()
+			
+	# Access the information by doing data_variable.process_id or data_variable.task_id etc..,
+	print ('Data received from client')
+        
+            
