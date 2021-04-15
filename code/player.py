738b7328882d9e4816d3083dd2236cfa8bc23f91
Mathieu SINDIC <mathieu.sindic@etudiant.univ-reims.fr>
2021-04-15 17:11:30 +0200

    Ajout de certaine fonction basique des zones du plateau

--- code/player.py
+++ code/player.py
@@ -2,11 +2,11 @@
 
 class Player:
 	def __init__(self, life, deck, graveyard, hand, battleground):
-		self.__life			= life
-		self.__deck			= deck
-		self.__graveyard	= graveyard
-		self.__hand			= hand
-		self.__battleground	= battleground
+		self._life			 = life
+		self._deck			 = deck
+		self._graveyard	     = graveyard
+		self._hand			 = hand
+		self._battleground	 = battleground
 
 
 class HumanPlayer(Player):
