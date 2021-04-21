1b59079ddb81816125fb69f67cd31c62baf4a96f
seb <sebastien.boissier@etudiant.univ-reims.fr>
2021-04-21 10:19:23 +0200

    to_debug

--- code/game.py
+++ code/game.py
@@ -4,33 +4,33 @@ from card import Card
 import socket
 #from player import Player
 class Game:
-	def __init__(self, server_address, server_port):
-		self.__socket	= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	def __init__(self, server_address, server_port, nb_client=2):
+		self.__socket	= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.__info		= (server_address, server_port)
-
+		self.__nb_client= nb_client
+		self.__conn 	= None
 	def get_socket(self):
 		return self.__socket
 
 	def wait_client(self):
 		self.__socket.bind(self.__info)
-		self.__socket.listen(5)
-		nb_client =2
-		conn 	= [0]*nb_client
-		address = [0]*nb_client
+		self.__conn = [0]*self.__nb_client
+		address 	= [0]*self.__nb_client
 		i=0
-		while i<nb_client:
+		while i<self.__nb_client:
 			i+=1
 			conn[i], address[i] = self.__socket.accept()
 			print('Connected by', address)
 
 	def start(self):
 		while True:
-			data = game.get_socket().recv(4096)
-			if not data:
-				break
-			data_variable = pickle.loads(data)
-			print('Data received from client :')
-			print(data_variable)
-			game.get_socket().send(data)
+			for i in range(self.__nb_client):
+				data = conn[i].get_socket().recv(4096)
+				if not data:
+					break
+				data_variable = pickle.loads(data)
+				print('Data received from client :')
+				print(data_variable)
+				conn[i].get_socket().send(data)
 
 
