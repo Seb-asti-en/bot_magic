a89260320c90d5abcabdf64df28e280efc8ba621
Mathieu SINDIC <mathieu.sindic@etudiant.univ-reims.fr>
2021-04-26 14:42:05 +0200

    Ajout des champs subtype, supertype et collection, la requete actuel retourne les cartes de la collection Arena Beginner Set

--- code/card.py
+++ code/card.py
@@ -4,15 +4,32 @@ import os
 import sys
 import pymysql # Installer le module avec pip
 
+#Variable global
+ID 				= 0
+NAME 			= 1
+COLOR 			= 2
+MANA_COST 		= 3
+IDENTITY 		= 4
+TEXT 			= 5
+POWER 			= 6
+TOUGHNESS 		= 7
+TYPE 			= 8
+SUBTYPE 		= 9
+SUPERTYPE 		= 10
+COLLECTION  	= 11
+
 def main():
 
 	privilege 		= ""
 	status			= None
 	db 				= None
 	curs 			= None
-	row_count 		= None
+	cardlen 		= 0
+#	row_count 		= 0
 	all_db_cards 	= None
 	card 			= []
+	
+	
 
 	# Lancement du serveur MySQL local
 	if sys.platform.startswith('darwin'):
@@ -38,16 +55,22 @@ def main():
 
 	# Récupération des cartes (45 premières)
 	curs.execute("""
-				 SELECT DISTINCT car_id, car_name, car_colors, car_manacost, car_coloridentity, car_text, car_power, car_toughness, cty_id, cty_name
-				 FROM mag_setcard, mag_set, mag_card, mag_cardtypeli, mag_cardtype
-				 WHERE set_id = sca_set 
-				 AND sca_card = car_id
-				 AND ctyl_card = car_id
-				 AND ctyl_type = cty_id
-				 ORDER BY car_id
+				 SELECT DISTINCT car_id, car_name, car_colors, car_manacost, car_coloridentity, car_text, car_power, car_toughness, cty_name, cst_name, csu_name, set_name
+				 FROM mag_card
+				 LEFT JOIN mag_cardtypeli ON ctyl_card = car_id
+				 LEFT JOIN mag_cardtype ON cty_id = ctyl_type
+				 LEFT JOIN mag_cardsubtypeli ON cstl_card = car_id
+				 LEFT JOIN mag_cardsubtype ON cst_id = cstl_subtype
+				 LEFT JOIN mag_cardsupertypeli ON csul_card = car_id
+				 LEFT JOIN mag_cardsupertype ON csu_id = csul_supertype
+				 INNER JOIN mag_setcard ON sca_card = car_id
+				 INNER JOIN mag_set ON set_id = sca_set
+				 WHERE set_name = "Arena Beginner Set"
+				 ORDER BY car_id 
 				 """)
-	row_count = 45
-	all_db_cards = curs.fetchmany(row_count)
+	
+	#row_count = 100
+	all_db_cards = curs.fetchall()
 
 	# Deconnexion de la DB
 	db.close()
@@ -60,37 +83,60 @@ def main():
 		os.system("mysql.server stop")
 	elif sys.platform.startswith('linux'):
 		os.system("sudo /etc/init.d/mysql stop")
-
-	# Créations des objets Card
-	for x in all_db_cards:
-		if(x[8] == 3):
-			card.append(CreatureCard(x))
-		if(x[8] == 14):
-			card.append(SorceryCard(x))
-		if(x[8] == 8):
-			card.append(LandCard(x))
-		if(x[8] == 7):
-			card.append(InstantCard(x))
+		
+	#Rentre les cartes sorti de la BD dans une liste, gère à moitié les double (problème niveau collection)
+	for buffer_card in all_db_cards:
+		if(cardlen > 0):
+			if(card[cardlen-1]._id == buffer_card[ID]):
+				card[cardlen-1].add_subtype(buffer_card[11])
+			else:
+				if(buffer_card[TYPE] == "Creature"):
+					card.append(CreatureCard(buffer_card))
+					cardlen += 1
+				elif(buffer_card[TYPE] == "Instant"):
+					card.append(InstantCard(buffer_card))
+					cardlen += 1
+				elif(buffer_card[TYPE] == "Land"):
+					card.append(LandCard(buffer_card))
+					cardlen += 1
+				elif(buffer_card[TYPE] == "Sorcery"):
+					card.append(SorceryCard(buffer_card))
+					cardlen += 1
+		else:
+			if(buffer_card[TYPE] == "Creature"):
+				card.append(CreatureCard(buffer_card))
+				cardlen += 1
+			elif(buffer_card[TYPE] == "Instant"):
+				card.append(InstantCard(buffer_card))
+				cardlen += 1
+			elif(buffer_card[TYPE] == "Land"):
+				card.append(LandCard(buffer_card))
+				cardlen += 1
+			elif(buffer_card[TYPE] == "Sorcery"):
+				card.append(SorceryCard(buffer_card))
+				cardlen += 1
+		
 		
 	# Affichage
 	for i in card:
 		print(i.to_string())
+	print("\nNombre de cartes obtenu : " + str(len(card)))
 
 class Card:
 
 	def __init__(self,card):
 
-		self._id			= card[0]
-		self._collection 	= ''
-		self._name			= card[1]
-		self._supertype		= ''
-		self._subtype		= ''
+		self._id			= card[ID]
+		self._collection 	= card[COLLECTION]
+		self._name			= card[NAME]
+		self._supertype		= card[SUPERTYPE]
+		self._subtype		= card[SUBTYPE]
 		self._init_colors(card)
 		self._init_mana_cost(card)
 		self._init_identity(card)
-		self._text			= ''
+		self._text			= card[TEXT]
 		self._effects		= ''
-		self._type 		= card[9]
+		self._type 			= card[TYPE]
 		
 	def get_id(self):
 		return self._id
@@ -99,8 +145,8 @@ class Card:
 	def _init_mana_cost(self, card):
 		#Initialise le cout en mana à 0
 		self._mana_cost = {'X' : 0,'C' : 0, 'W' : 0, 'B' : 0, 'R' : 0, 'G' : 0, 'U' : 0}
-		#Récup le cout en string | card[3] == mana cost dans la table mag_card
-		temp = card[3]
+		#Récup le cout en string | card[MANA_COST] == mana cost dans la table mag_card
+		temp = card[MANA_COST]
 		#Transforme le string en liste
 		res = temp.strip('}{').split('}{')
 		#Si il a un cout X de base
@@ -116,38 +162,48 @@ class Card:
 	
 	def _init_colors(self, card):
 		self._colors = {'C' : 0, 'W' : 0, 'B' : 0, 'R' : 0, 'G' : 0, 'U' : 0}
-		temp = card[2]
+		temp = card[COLOR]
 		res = temp.split(';')
 		for x in res:
-			self._colors[x] = self._colors[x] + 1
+			if(x == ''):	
+				self._colors['C'] = 1
+			else:
+				self._colors[x] = 1
 	
 	def _init_identity(self, card):
 		self._identity = {'C' : 0, 'W' : 0, 'B' : 0, 'R' : 0, 'G' : 0, 'U' : 0}
-		temp = card[4]
+		temp = card[IDENTITY]
 		res = temp.split(';')
 		for x in res:
-			self._identity[x] = self._identity[x] + 1
+			if(x == ''):	
+				self._identity['C'] = 1
+			else:
+				self._identity[x] = 1
 		
-	def print_colors(self):
-		print(self._colors)
-		
-	def print_mana_cost(self):
-		print(self._mana_cost)
-		
-	def print_identity(self):
-		print(self._identity)
+	def add_subtype(self, subtype):
+		if(subtype == None):
+			pass
+		else:
+			self._subtype = self._subtype + ' and ' + subtype
 	
 	def to_string(self):
 
 		string = ""
 
-		string += "CARD TYPE : " + self._type + " \n" 
-		string += "Id : " + str(self._id) + " \n" 
-		#string += "Collection : " + str(self._collection) + "\n"
-		string += "Name : " + self._name + "\n"
-		#string += "Supertype and subtype : " + self._supertype + " " + self._subtype + "\n"
+		if(self._type == None):
+			if("token" in self._name):
+				string += "CARD TYPE : TOKEN \n"
+		else: 
+			string += "CARD TYPE : " + self._type + " \n" 
+		string += "ID : " + str(self._id) + " \n" 
+		string += "COLLECTION : " + str(self._collection) + "\n"
+		string += "NAME : " + self._name + "\n"
+		if(self._supertype != None):
+			string += "SUPERTYPE : " + self._supertype + "\n"
+		if(self._subtype != None):
+		    string += "SUBTYPE : " + self._subtype + "\n"
 		
-		string += "Color : "
+		string += "COLOR : "
 		if (self._colors['C'] == 1):
 			string += "Incolore"
 		else :
@@ -168,7 +224,7 @@ class Card:
 
 		string += "\n"
 		
-		string += "Mana cost : "
+		string += "MANA COST : "
 		if (self._mana_cost['X'] > 0):
 			string += str(self._mana_cost['X']) + "(Mana) "
 			
@@ -193,7 +249,7 @@ class Card:
 		string += "\n"
 		
 		
-		string += "Identity : "
+		string += "IDENTITY : "
 		if (self._identity['C'] == 1):
 			string += "Incolore"
 		else :
@@ -214,7 +270,10 @@ class Card:
 
 		string += "\n"
 		
-		#string += "Text : " + self._text + "\n"
+		if(self._text != None):
+			string += "TEXT : " + self._text + "\n"
+		#if(self._effects != None):
+			#string += "EFFECT : " + str(self._effects) + "\n"
 		return string
 
 
@@ -222,13 +281,13 @@ class CreatureCard(Card):
 
 	def __init__(self, card):
 		super().__init__(card)
-		self.__power = card[6]
-		self.__toughness = card[7]
+		self.__power = card[POWER]
+		self.__toughness = card[TOUGHNESS]
 		
 	def to_string(self):
 		string = super().to_string()
-		string += "Power : " + str(self.__power) + " \n" 
-		string += "Toughness : " + str(self.__toughness) + "\n"
+		string += "POWER : " + str(self.__power) + " \n" 
+		string += "TOUGHNESS : " + str(self.__toughness) + "\n"
 		return string
 
 class SorceryCard(Card):
