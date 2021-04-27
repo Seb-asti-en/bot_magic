ad0260b14d9c26412aa1b9b4c7242344680d99f2
Mathieu SINDIC <mathieu.sindic@etudiant.univ-reims.fr>
2021-04-27 15:01:39 +0200

    Ajout de la classe effect

---
+++ code/effect.py
@@ -0,0 +1,55 @@
+class Effect:
+	
+	def __init__(self,name):
+		self.__name	= name
+		
+	def deathtouch(self):
+		pass
+	
+	def defender(self):
+		pass
+	
+	def double_strike(self):
+		pass
+	
+	def first_strike(self):
+		pass
+	
+	def flash(self):
+		pass
+	
+	def flying(self):
+		pass
+	
+	def haste(self):
+		pass
+	
+	def hexproof(self):
+		pass
+	
+	def indestructible(self):
+		pass
+	
+	def lifelink(self):
+		pass
+	
+	def menace(self):
+		pass
+	
+	def protection(self):
+		pass
+	
+	def reach(self):
+		pass
+	
+	def trample(self):
+		pass
+	
+	def vigilance(self):
+		pass
+	
+	def prowess(self):
+		pass
+	
+	def skulk(self):
+		pass
